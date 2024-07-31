aa76b2273776188c42774bdf587497f8f9959d69
Jonathan de Paula <jonathanpaula22@gmail.com>
2024-07-31 13:19:36 +0200

    After giving proper though I decided to not move forward with the reactive approach. Changing the pom.xml to achieve this goal.

--- src/main/java/com/wearewaes/simple_bank_account/SimpleBankAccountApplication.java
+++ src/main/java/com/wearewaes/simple_bank_account/SimpleBankAccountApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleBankAccountApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleBankAccountApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankAccountApplication.class, args);
+    }
 
 }
